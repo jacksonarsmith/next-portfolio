3c227d87c90ea446ee439d66ea198476749c314f
Jackson Smith <jackson16smith@gmail.com>
2023-11-19 00:14:03 -0500

    Added basic boilerplate styling for Contact form, handling on the form is in need to be implemented still.

--- app/components/contact/Contact.jsx
+++ app/components/contact/Contact.jsx
@@ -4,12 +4,20 @@ export default function Contact() {
   return (
     <div id='contact' className={styles.container}>
       <h1>Get in touch</h1>
+      <div className={styles.line}></div>
+      <p>
+        I'm here to explore opportunities with you. Whether you're seeking a new challenge, 
+        interested in a collaboration, or have a specific role in mind, I'd love to hear from 
+        you. Use the form below to reach out, and let's start a conversation about how we can 
+        create success together!
+      </p>
       <div className={styles.form}>
         <div className={styles.grid_two}>
           <input type="text" placeholder='Enter Name'/>
           <input type="email" id="email" placeholder='Enter email'/>
         </div>
-        <input type="text" />
+        <input type="text" placeholder='Message' className={styles.message}/>
+        <button disabled="disabled">Send Message</button>
       </div>
     </div>
   )
--- app/components/testimonials/Testimonials.jsx
+++ app/components/testimonials/Testimonials.jsx
@@ -4,6 +4,7 @@ export default function Testimonials() {
   return (
     <div id='testimonials' className={styles.container}>
       <h1>Testimonials</h1>
+      <div className={styles.line}></div>
       <div className={styles.testimonials}>
         <div className={styles.testimonial}>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero perferendis numquam id, molestiae molestias aliquid, delectus fugiat rerum beatae odio ea laboriosam tempora sequi praesentium autem tenetur, qui aut fugit.</p>
