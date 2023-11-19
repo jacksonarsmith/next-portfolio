64a9fd96fc6ae69aeb4e09d4072b2ac5aa540c7f
Jackson Smith <jackson16smith@gmail.com>
2023-11-18 19:54:34 -0500

    Hero section boilerplate implemented, and basis for skills section.

--- app/components/hero/Hero.jsx
+++ app/components/hero/Hero.jsx
@@ -1,7 +1,22 @@
 import styles from './hero.module.css'
 
+import Image from 'next/image'
+
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <div className={styles.container}>
+      <Image src="/hero.jpeg" width={6000} height={4000} alt="Picture of the Jackson" className={styles.image}/>
+
+      <div className={styles.description}>
+        <h1>Software Engineer</h1>
+        <div className={styles.line}></div>
+        <p>
+          Hello! I'm Jackson Smith, a dedicated software engineer passionate about translating creative ideas into impactful solutions. 
+          I specialize in the entire software development life cycle, from designing user-friendly interfaces to architecting scalable 
+          back-end systems. My focus is on problem-solving, aiming to address real-world challenges with elegant and efficient solutions. 
+          As you explore my portfolio, you'll find a showcase of projects reflecting my commitment to excellence and innovation. 
+        </p>
+      </div>
+    </div>
   )
 }
--- app/components/skills/Skills.jsx
+++ app/components/skills/Skills.jsx
@@ -1,7 +1,50 @@
+'use client'
+
 import styles from './skills.module.css'
+import { useState } from 'react'
 
 export default function Skills() {
+  const [selectedItem, setSelectedItem] = useState(0)
+
+  const handleItemSelection = (index) => {
+    setSelectedItem(index); 
+  }
   return (
-    <div>Skills</div>
+    <div className={styles.container}>
+      <div className={styles.selection}>
+        <ul>
+          <li>
+            Frontend Technologies
+          </li>
+          <li>
+            Backend Technologies
+          </li>
+          <li>
+            Development Tools & Strategies
+          </li>
+        </ul>
+      </div>
+      <div className={styles.columns}>
+        <ul>
+          <div className={styles.frontend}>
+            <h3>Frontend Tech Stack</h3>
+            <ul>
+              <li>
+                <h4>Frontend Languages</h4>
+              </li>
+              <li>
+                <h4>Frontend Frameworks</h4>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.backend}>
+
+          </div>
+          <div className={styles.development}>
+
+          </div>
+        </ul>
+      </div>
+    </div>
   )
 }
