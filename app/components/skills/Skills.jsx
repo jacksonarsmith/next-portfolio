b7a24da1ec48a2f7bfcdcb7deab77c367aa780a8
Jackson Smith <jackson16smith@gmail.com>
2023-11-18 23:22:44 -0500

    Added skills section and styling, still some info to be added.

--- app/components/skills/Skills.jsx
+++ app/components/skills/Skills.jsx
@@ -1,7 +1,12 @@
 'use client'
 
 import styles from './skills.module.css'
+import '@fortawesome/fontawesome-svg-core'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faJsSquare } from '@fortawesome/free-brands-svg-icons'
 import { useState } from 'react'
+import Image from 'next/image'
 
 export default function Skills() {
   const [selectedItem, setSelectedItem] = useState(0)
@@ -9,41 +14,63 @@ export default function Skills() {
   const handleItemSelection = (index) => {
     setSelectedItem(index); 
   }
+
   return (
     <div className={styles.container}>
       <div className={styles.selection}>
         <ul>
-          <li>
-            Frontend Technologies
+          <li onClick={() => handleItemSelection('frontend')}>
+            Front-end Technologies
           </li>
-          <li>
-            Backend Technologies
+          <li onClick={() => handleItemSelection('backend')}>
+            Back-end Technologies
           </li>
-          <li>
-            Development Tools & Strategies
+          <li onClick={() => handleItemSelection('development')}>
+            Development Tools
           </li>
         </ul>
       </div>
       <div className={styles.columns}>
-        <ul>
-          <div className={styles.frontend}>
-            <h3>Frontend Tech Stack</h3>
-            <ul>
-              <li>
-                <h4>Frontend Languages</h4>
-              </li>
-              <li>
-                <h4>Frontend Frameworks</h4>
-              </li>
-            </ul>
+        {selectedItem === 'frontend' && (
+        <div className={styles.frontend}>
+          <Image src='/html5.png' width={480} height={480} alt='HTML5 icon picture' className={styles.image}/>
+          <div className={styles.grid_three}>
+            <Image src='/react.png' width={480} height={480} alt='Reactjs icon picture' className={styles.image}/>
+            <Image src='/javascript.png' width={480} height={480} alt='Javascript icon picture' className={styles.image}/>
+            <Image src='/nextjs.png' width={480} height={480} alt='Nextjs icon picture' className={styles.image}/>
           </div>
+          <Image src='/css3.png' width={480} height={480} alt='CSS3 icon picture' className={styles.image}/>
+        </div>
+        )}
+        {selectedItem === 'backend' && (
           <div className={styles.backend}>
-
+            <div className={styles.grid_two}>
+              <Image src='/python.png' width={480} height={480} alt='Python icon picture' className={styles.image}/>
+              <Image src='/java.png' width={480} height={480} alt='Java icon picture' className={styles.image}/>
+            </div>
+            <div className={styles.grid_three}>
+              <Image src='/javascript.png' width={480} height={480} alt='Javascript icon picture' className={styles.image}/>
+              <Image src='/nodejs.png' width={480} height={480} alt='Nodejs icon picture' className={styles.image}/>
+              <Image src='/expressjs.png' width={480} height={480} alt='Expressjs icon picture' className={styles.image}/>
+            </div>
+            <div className={styles.grid_two}>
+              <Image src='/cpp.png' width={480} height={480} alt='C++ icon picture' className={styles.image}/>
+              <Image src='/mongodb.png' width={480} height={480} alt='MongoDB icon picture' className={styles.image}/>
+            </div>
           </div>
+        )}
+        {selectedItem === 'development' && (
           <div className={styles.development}>
-
+            <div className={styles.grid_two}>
+              <Image src='/npm.png' width={480} height={480} alt='NPM icon picture' className={styles.image}/>
+              <Image src='/git.png' width={480} height={480} alt='Git icon picture' className={styles.image}/>
+            </div>
+            <div className={styles.grid_two}>
+              <Image src='/jira.png' width={480} height={480} alt='Jira icon picture' className={styles.image}/>
+              <Image src='/figma.png' width={480} height={480} alt='Figma icon picture' className={styles.image}/>
+            </div>
           </div>
-        </ul>
+        )}
       </div>
     </div>
   )
