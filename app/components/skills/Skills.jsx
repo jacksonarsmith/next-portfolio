3c75734d4bacb6dd898b0c609e4fbefc20b171d4
Jackson Smith <jackson16smith@gmail.com>
2024-01-18 18:07:32 -0500

    Small style change for the skills section.

--- app/components/skills/Skills.jsx
+++ app/components/skills/Skills.jsx
@@ -33,41 +33,89 @@ export default function Skills() {
       <div className={styles.columns}>
         {selectedItem === 'frontend' && (
         <div className={styles.frontend}>
-          <Image src='/html5.png' width={480} height={480} alt='HTML5 icon picture' className={styles.image} priority={false}/>
-          <div className={styles.grid_three}>
-            <Image src='/react.png' width={480} height={480} alt='Reactjs icon picture' className={styles.image} priority={false}/>
-            <Image src='/javascript.png' width={480} height={480} alt='Javascript icon picture' className={styles.image} priority={false}/>
-            <Image src='/nextjs.png' width={480} height={480} alt='Nextjs icon picture' className={styles.image} priority={false}/>
+          <div className={styles.imageContainer}>
+            <Image src='/html5.png' width={480} height={480} alt='HTML5 icon picture' className={styles.image} priority={false}/>
+            <div className={styles.imageText}>HTML5</div>
           </div>
-          <Image src='/css3.png' width={480} height={480} alt='CSS3 icon picture' className={styles.image} priority={false}/>
+          <div className={styles.grid_three}>
+            <div className={styles.imageContainer}>
+              <Image src='/react.png' width={480} height={480} alt='Reactjs icon picture' className={styles.image} priority={false}/>
+              <div className={styles.imageText}>React</div>
+            </div>
+            <div className={styles.imageContainer}>
+              <Image src='/javascript.png' width={480} height={480} alt='Javascript icon picture' className={styles.image} priority={false}/>
+              <div className={styles.imageText}>JavaScript</div>
+            </div>
+            <div className={styles.imageContainer}>
+              <Image src='/nextjs.png' width={480} height={480} alt='Nextjs icon picture' className={styles.image} priority={false}/>
+              <div className={styles.imageText}>Next.js</div>
+            </div>
+          </div>
+          <div className={styles.imageContainer}>
+            <Image src='/css3.png' width={480} height={480} alt='CSS3 icon picture' className={styles.image} priority={false}/>
+            <div className={styles.imageText}>CSS3</div>
+          </div>
         </div>
         )}
         {selectedItem === 'backend' && (
           <div className={styles.backend}>
             <div className={styles.grid_two}>
-              <Image src='/python.png' width={480} height={480} alt='Python icon picture' className={styles.image} priority={false}/>
-              <Image src='/java.png' width={480} height={480} alt='Java icon picture' className={styles.image} priority={false}/>
+              <div className={styles.imageContainer}>
+                <Image src='/python.png' width={480} height={480} alt='Python icon picture' className={styles.image} priority={false}/>
+                <div className={styles.imageText}>Python</div>
+              </div>
+              <div className={styles.imageContainer}>
+                <Image src='/java.png' width={480} height={480} alt='Java icon picture' className={styles.image} priority={false}/>
+                <div className={styles.imageText}>Java</div>
+              </div>
             </div>
             <div className={styles.grid_three}>
-              <Image src='/javascript.png' width={480} height={480} alt='Javascript icon picture' className={styles.image} priority={false}/>
-              <Image src='/nodejs.png' width={480} height={480} alt='Nodejs icon picture' className={styles.image} priority={false}/>
-              <Image src='/expressjs.png' width={480} height={480} alt='Expressjs icon picture' className={styles.image} priority={false}/>
+              <div className={styles.imageContainer}>
+                <Image src='/javascript.png' width={480} height={480} alt='Javascript icon picture' className={styles.image} priority={false}/>
+                <div className={styles.imageText}>JavaScript</div>
+              </div>
+              <div className={styles.imageContainer}>
+                <Image src='/nodejs.png' width={480} height={480} alt='Nodejs icon picture' className={styles.image} priority={false}/>
+                <div className={styles.imageText}>Node.js</div>
+              </div>
+              <div className={styles.imageContainer}>
+                <Image src='/expressjs.png' width={480} height={480} alt='Expressjs icon picture' className={styles.image} priority={false}/>
+                <div className={styles.imageText}>Express.js</div>
+              </div>
             </div>
             <div className={styles.grid_two}>
-              <Image src='/cpp.png' width={480} height={480} alt='C++ icon picture' className={styles.image} priority={false}/>
-              <Image src='/mongodb.png' width={480} height={480} alt='MongoDB icon picture' className={styles.image} priority={false}/>
+              <div className={styles.imageContainer}>
+                <Image src='/cpp.png' width={480} height={480} alt='C++ icon picture' className={styles.image} priority={false}/>
+                <div className={styles.imageText}>C++</div>
+              </div>
+              <div className={styles.imageContainer}>
+                <Image src='/mongodb.png' width={480} height={480} alt='MongoDB icon picture' className={styles.image} priority={false}/>
+                <div className={styles.imageText}>MongoDB</div>
+              </div>
             </div>
           </div>
         )}
         {selectedItem === 'development' && (
           <div className={styles.development}>
             <div className={styles.grid_two}>
-              <Image src='/npm.png' width={480} height={480} alt='NPM icon picture' className={styles.image} priority={false}/>
-              <Image src='/git.png' width={480} height={480} alt='Git icon picture' className={styles.image} priority={false}/>
+              <div className={styles.imageContainer}>
+                <Image src='/npm.png' width={480} height={480} alt='NPM icon picture' className={styles.image} priority={false}/>
+                <div className={styles.imageText}>NPM</div>
+              </div>
+              <div className={styles.imageContainer}>
+                <Image src='/git.png' width={480} height={480} alt='Git icon picture' className={styles.image} priority={false}/>
+                <div className={styles.imageText}>Git</div>
+              </div>
             </div>
             <div className={styles.grid_two}>
-              <Image src='/jira.png' width={480} height={480} alt='Jira icon picture' className={styles.image} priority={false}/>
-              <Image src='/figma.png' width={480} height={480} alt='Figma icon picture' className={styles.image} priority={false}/>
+              <div className={styles.imageContainer}>
+                <Image src='/jira.png' width={480} height={480} alt='Jira icon picture' className={styles.image} priority={false}/>
+                <div className={styles.imageText}>Jira</div>
+              </div>
+              <div className={styles.imageContainer}>
+                <Image src='/figma.png' width={480} height={480} alt='Figma icon picture' className={styles.image} priority={false}/>
+                <div className={styles.imageText}>Figma</div>
+              </div>
             </div>
           </div>
         )}
