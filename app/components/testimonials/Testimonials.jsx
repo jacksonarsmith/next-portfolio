028b74cc67ec680b2eb6a52d6f5a2fc35a6ee9d5
Jackson Smith <jackson16smith@gmail.com>
2024-01-29 12:23:45 -0500

    Final change for prod this sprint.

--- app/components/testimonials/Testimonials.jsx
+++ app/components/testimonials/Testimonials.jsx
@@ -9,7 +9,7 @@ export default function Testimonials() {
       <div className={styles.testimonials}>
         <div className={styles.testimonial}>
           <Image src='/testimonial_jacob.jpeg' width={480} height={480} alt='Jacob Testimonial Picture' className={styles.image} priority={false}/>
-          <p>I highly recommend Jackson for their exceptional programming skills. Their proficiency in diverse languages, ability to solve complex problems, and collaborative mindset make them an invaluable asset to any team. Jackson consistently delivers high-quality solutions and is an outstanding contributor to any programming project.</p>
+          <p>I highly recommend Jackson for their exceptional programming skills. His proficiency in diverse languages, ability to solve complex problems, and collaborative mindset make him an invaluable asset to any team. Jackson consistently delivers high-quality solutions and is an outstanding contributor to any programming project.</p>
         </div>
       </div>
     </div>
