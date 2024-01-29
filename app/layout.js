0ccd181f6b05d167e342c8c785db937ea9726c06
Jackson Smith <jackson16smith@gmail.com>
2024-01-29 11:57:55 -0500

    Testing different meta tags for prod.

--- app/layout.js
+++ app/layout.js
@@ -1,24 +1,19 @@
 import { Inter } from 'next/font/google'
-import Head from 'next/head'
+//import Head from 'next/head'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
-
+/*
 export const metadata = {
   title: "Jackson Smith's Portfolio",
   description: 'Portfolio website built with Next.js by Software Engineer Jackson Smith',
   
-}
+}*/
 
 export default function RootLayout({ children }) {
   return (
-    <>
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+    <html lang="en">
       <body className={inter.className}>{children}</body>
-    </>
+    </html>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Image from 'next/image'
 import styles from './page.module.css'
 import Navbar from '@/app/components/navigation/Navigation'
@@ -11,6 +12,11 @@ import Footer from '@/app/components/footer/Footer'
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Head>
+        <title>Your Page Title</title>
+        <meta name="description" content="Your Page Description" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Navbar />
       <Hero />
       <Skills />
