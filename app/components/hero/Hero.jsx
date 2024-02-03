129244a07352a5dab4d151af88edf4fcdf6185c5
Jackson Smith <jackson16smith@gmail.com>
2024-02-02 22:04:03 -0500

    Recent changes to hero section.

--- app/components/hero/Hero.jsx
+++ app/components/hero/Hero.jsx
@@ -1,20 +1,28 @@
 import styles from './hero.module.css'
-
+import Link from 'next/link'
 import Image from 'next/image'
 
 export default function Hero() {
   return (
     <div className={styles.container}>
-      <Image src="/hero.jpeg" width={6000} height={4000} alt="Picture of the Jackson" className={styles.image} priority={true}/>
-
-      <div className={styles.description}>
-        <h1>Software Engineer</h1>
-        <hr />
+      <div className={styles.card}>
+          <Image src="/background-hero.png" width={3000} height={2000} alt="Picture of the Jackson" className={styles.image} priority={true}/>
+      </div>
+      <div className={styles.button}>
+          <div className={styles.box}>J</div>
+          <div className={styles.box}>A</div>
+          <div className={styles.box}>C</div>
+          <div className={styles.box}>K</div>
+          <div className={styles.box}>S</div>
+          <div className={styles.box}>O</div>
+          <div className={styles.box}>N</div>
+      </div>
+      <div className={styles.text}>
         <p>
-          Hello! I&apos;m Jackson Smith, a dedicated software engineer passionate about translating creative ideas into impactful solutions. 
-          I specialize in the entire software development life cycle, from designing user-friendly interfaces to architecting scalable 
-          back-end systems. My focus is on problem-solving, aiming to address real-world challenges with elegant and efficient solutions. 
-          As you explore my portfolio, you&apos;ll find a showcase of projects reflecting my commitment to excellence and innovation. 
+            Hello! I&apos;m Jackson Smith, a dedicated software engineer passionate about translating creative ideas into impactful solutions. 
+            I specialize in the entire software development life cycle, from designing user-friendly interfaces to architecting scalable 
+            back-end systems. My focus is on problem-solving, aiming to address real-world challenges with elegant and efficient solutions. 
+            As you explore my portfolio, you&apos;ll find a showcase of projects reflecting my commitment to excellence and innovation. 
         </p>
       </div>
     </div>
